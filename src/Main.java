6541392ad6c0e8c216c1b70526f21d2d72bb254b
dryabinin <haschish@yandex.ru>
2020-09-17 12:15:57 +0500

    byal

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,6 @@ public class Main {
         System.out.printf("Shape with max area: %s \n", maxShape);
     }
 
-
     private static void printArrayElements(Object[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(i + ": " + arr[i]);
